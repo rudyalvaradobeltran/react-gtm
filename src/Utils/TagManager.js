64c9dd7200afa624b74b45b232c5e74d9ba0cd06
rudyalvaradobeltran <rudy.alvarado.b@gmail.com>
2022-03-22 19:34:22 -0300

    Tag Manager create DataLayer

--- src/Utils/TagManager.js
+++ src/Utils/TagManager.js
@@ -1,1 +1,19 @@
-import TagManager from 'react-gtm-module';
+import TagManager from "react-gtm-module";
+
+export const args = {
+  gtmId: "",
+};
+
+export const init = (args) => {
+  TagManager.initialize({ args });
+};
+
+export const createDataLayer = (event, path, user) => {
+  TagManager.dataLayer({
+    dataLayer: {
+      event,
+      path,
+      user,
+    },
+  });
+};
